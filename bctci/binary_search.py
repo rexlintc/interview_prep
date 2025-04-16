deaf14bd6d772eea15b5b2b3527bb24767a21e68
Rex Lin <rexlintc@gmail.com>
2025-04-15 22:19:11 -0700

    

---
+++ bctci/binary_search.py
@@ -0,0 +1,34 @@
+# Problem 29.1 Search in sorted array
+"""
+Given a sorted array of integers, arr, and a target value, target, return the target's index if it exists in the
+array or -1 if it doesn't.
+▶ Example: arr = [-2, 0, 3, 4, 7, 9, 11], target = 3
+Output: 2.
+▶ Example: arr = [-2, 0, 3, 4, 7, 9, 11], target = 2
+Output: -1.
+"""
+
+# Git Commits
+"""
+Find the first commit that fails a test in a sequence of Git commits. We know the test was passing for every commit until it started failing at some point.
+["pass", "pass", "pass", "pass", "fail", "fail", "fail"]
+"""
+"""
+Target: first fail
+target_index - 1 = pass and target_index + 1 = fail
+Before region = before first fail
+After region = after first fail
+"""
+
+# Squared Target
+"""
+Given a sorted array of positive integers and a target value, find the largest number in the array that can be squared and still be less than or equal to the target, if any. Return the number (not its index).
+[2, 3, 4, 5, 6, 7, 8, 11, 20, 21, 23, 25, 25], target = 36
+"""
+"""
+Target: largest number that when squared is still less than target
+Before region: 
+After region:
+"""
+
+# First non-negative
